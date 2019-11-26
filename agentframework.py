efd146543f3e9951179623f5bb6d05a5ba6df815
SideshowTee <56164084+SideshowTee@users.noreply.github.com>
2019-11-26 14:25:02 +0000

    ABM upload

---
+++ agentframework.py
@@ -0,0 +1,62 @@
+import random
+class Agent(): # Create class
+    
+    # Add functions to class. Init function runs whenever new agent is created.
+    def __init__(self, environment, agents, y, x):
+        # Create x from web scrape data, use random if missing x value
+        if (x == None):
+            self.x = random.randint(0,100)
+        else:
+            self.x = x
+        # Create y from web scrape data, use random if missing y value
+        if (y == None):
+            self.y = random.randint(0,100)
+        else:
+            self.y = y
+        self.environment = environment
+        self.agents = agents
+        self.store = 0 # How much each agent has "stored/eaten" at the start
+    
+     
+    def __str__(self):
+        """ For representing this as a string
+        Returns:
+            A string representation
+        """
+        return "x=" + str(self.x) + ", y=" + str(self.y) + ", store " + str(self.store) # X coord, Y coord and storage amount
+    
+    # Function to randomly move the agents.
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100
+
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
+
+    # Function to "eat" and store the environment.            
+    def eat(self): 
+        if self.environment[self.y][self.x] > 10: # If RGB/pixel value (in.txt) greater than 10 (they all are)
+            self.environment[self.y][self.x] -= 10 # Reduce value by 10 for "in.txt" RGB values / changes the environment colour
+            self.store += 10 # Adds 10 to store/amount eaten for each agent
+        
+            
+    # Function to share storage with neighbours
+    def share_with_neighbours(self, neighbourhood):
+        for agent in self.agents: # Loop through the agents in self.agents            
+            distance = self.distance_between(agent) # Calculate the distance between self and the current other agent:            
+            if distance <= neighbourhood: # If distance is less than or equal to the neighbourhood                
+                sum = self.store + agent.store # Sum self.store and agent.store               
+                ave = sum /2 # Divide sum by two to calculate average                
+                self.store = ave # self.store = average                
+                agent.store = ave # agent.store = average
+                #print("sharing " + str(dist) + " " + str(ave))
+
+    # Function to create pythag formula to calculate distance between co-ordinates
+    def distance_between(self, agent):
+        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
+                   
+    
---
+++ model.py
@@ -0,0 +1,157 @@
+import agentframework
+import csv
+import matplotlib
+import tkinter as tk
+matplotlib.use('TkAgg')
+import matplotlib.pyplot
+import matplotlib.animation
+import random
+import requests
+import bs4
+
+
+num_of_moves = 2 # variable for how many times agent moves before refreshing
+num_of_iterations = 100 # variable for total number of iterations between each refresh
+neighbourhood = 20
+agents = []
+
+
+# Download and print y and x data from webpage
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+print(td_ys)
+print(td_xs)
+
+
+# Read in file and assign to 'dataset' variable
+f = open('in.txt', newline='')
+dataset = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC) # Second argument converts to float
+
+
+environment = [] # Create empty list for environment
+
+
+for row in dataset: # Loop through dataset for a list of rows
+    rowlist = [] # Create empty list for row data
+    for value in row: # Loop through dataset for a list of values
+        rowlist.append(value) # Append values to the rowlist
+    environment.append(rowlist) # Append rowlist data into environment list
+        # print(value) # As Floats
+f.close()    
+   
+  
+carry_on = True
+
+
+def update(frame_number): # frame_number is a required parameter of the update function
+    
+    fig.clear() # Refreshes screen and removes previous frame
+    global carry_on # Defines global variable should be used
+    
+    # Move the agents then eat the grass (call move + eat functions).
+    for j in range(num_of_moves):
+        random.shuffle(agents) # Shuffles list of agents each iteration before behaviour functions
+        for i in range(num_of_agents):
+            agents[i].move()
+            agents[i].eat()
+            agents[i].share_with_neighbours(neighbourhood)
+             
+
+    # Loop through agents and set variable carry_on to false when all agents store > 500
+    count = 0
+    for i in range(num_of_agents):
+        if agents[i].store > 500:
+            count = count+1
+    if (count == num_of_agents):
+        print("Stopping Condition: All agents have exceeded store of 500")
+        for i in range(num_of_agents):
+            print(agents[i])
+            """ Testing that all agents exceed storage of 500
+            Returns:
+                Each agents coordinates and storage amount
+            """
+        carry_on = False
+
+    matplotlib.pyplot.xlim(0, 99)
+    matplotlib.pyplot.ylim(0, 99)
+    matplotlib.pyplot.imshow(environment)
+    
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x,agents[i].y, c='white', marker='*')
+
+  
+# Generator function to stop supply when stopping condition is met          
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not neccessary as no assignment is being performed, but provides clarity
+    while (a < num_of_iterations) & (carry_on) :
+        yield a			# Returns control and waits next call.
+        a = a + 1
+        
+      
+# Sets pop-up window/figure size
+fig = matplotlib.pyplot.figure(figsize=(10, 10)) 
+  
+# Create function to plot animation and passes in functions/variables
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
+    canvas.draw()
+    
+# Create function for setting the value of num_of_agents from slider bar with w.get()   
+def sel():
+    
+    global num_of_agents
+    global td_ys
+    global td_xs
+    global agents
+    num_of_agents = w.get()
+    # Pass in agents from web scrape, environment variable (and self as always).
+    for i in range(num_of_agents):
+        y = int(td_ys[i].text)
+        x = int(td_xs[i].text)
+        #print(x, y)
+        agents.append(agentframework.Agent(environment, agents, y, x))
+
+
+# Styling of the canvas  
+# Uses toolkit function within tkinter and assigns to root variable. Main window.
+root = tk.Tk() 
+root.wm_title("Model") # Sets title of GUI
+
+
+# Create menu in GUI
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tk.Menu(menu_bar)
+menu_bar.add_cascade(label="Options", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
+""" Select "Run Model"
+Returns:
+    Initiates the program
+""" 
+model_menu.add_command(label="Quit", command=root.destroy)
+""" Select "Quit"
+Returns:
+    Closes the program
+""" 
+
+# Creates scalebar
+w = tk.Scale(root,from_=10, to=100, orient=tk.HORIZONTAL, width=50, length=500, label="Number of Agents")
+w.pack()
+
+
+#model_menu.add_command(label="Number of agents", variable=num_of_agents)
+button = tk.Button(root, text="Set Scale Value", command=sel)
+button.pack()
+
+
+# Passes fig settings and root into master kwarg
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root) 
+canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1) 
+
+
+# Sets GUI to wait for events/interactions        
+tk.mainloop()
